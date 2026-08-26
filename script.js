342f4c41283192583a29b9416874078d72d63d0b
Prashant Kumar <prashant8252241631@gmail.com>
2026-08-27 00:46:21 +0530

    Create basic todo list

---
+++ script.js
@@ -0,0 +1,23 @@
+const taskInput = document.getElementById("taskInput");
+const addBtn = document.getElementById("addBtn");
+const taskList = document.getElementById("taskList");
+
+addBtn.addEventListener("click", function () {
+
+    const taskText = taskInput.value;
+
+    console.log("Task:", taskText);
+
+    if (taskText === "") {
+        alert("Please enter a task");
+        return;
+    }
+
+    const li = document.createElement("li");
+
+    li.textContent = taskText;
+
+    taskList.appendChild(li);
+
+    taskInput.value = "";
+});
