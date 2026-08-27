7a1f656b2d385d6e0b9e53265d590507f03af360
Prashant Kumar <prashant8252241631@gmail.com>
2026-08-27 23:59:51 +0530

    Add complete task functionality

--- script.js
+++ script.js
@@ -4,26 +4,46 @@ const taskList = document.getElementById("taskList");
 
 addBtn.addEventListener("click", function () {
 
-    const taskText = taskInput.value;
-
-    console.log("Task:", taskText);
+    const taskText = taskInput.value.trim();
 
     if (taskText === "") {
         alert("Please enter a task");
         return;
     }
 
+    // Create li
     const li = document.createElement("li");
 
-    li.textContent = taskText;
+    // Create task text
+    const taskSpan = document.createElement("span");
+    taskSpan.textContent = taskText;
 
+    // Create Complete button
+    const completeBtn = document.createElement("button");
+    completeBtn.textContent = "Complete";
+
+    // Complete button functionality
+    completeBtn.addEventListener("click", function () {
+        taskSpan.classList.toggle("completed");
+    });
+
+    // Create Delete button
     const deleteBtn = document.createElement("button");
-
     deleteBtn.textContent = "Delete";
 
+    // Delete button functionality
+    deleteBtn.addEventListener("click", function () {
+        li.remove();
+    });
+
+    // Add everything to li
+    li.appendChild(taskSpan);
+    li.appendChild(completeBtn);
     li.appendChild(deleteBtn);
 
+    // Add li to task list
     taskList.appendChild(li);
 
+    // Clear input
     taskInput.value = "";
 });
